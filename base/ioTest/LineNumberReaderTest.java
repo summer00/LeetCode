2810e713f4f9df66487071edae5b2109cc557efd
xia <xiaright00@sina.com>
2015-09-12 23:19:38 +0800

    v1.0 add:简单实现lineNumberReader

---
+++ base/ioTest/LineNumberReaderTest.java
@@ -0,0 +1,71 @@
+package ioTest;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.Reader;
+
+import org.junit.Test;
+
+public class LineNumberReaderTest {
+	@Test
+	public void setLineTest() {
+		try (LineNumberReader lnr = new LineNumberReader(new FileReader("F:\\study\\9.JAVA_BASE\\笔记\\test.txt"))) {
+			String line;
+			lnr.setLineNumber(100); // 设置行号
+			while ((line = lnr.readLine()) != null) {
+				System.out.println(lnr.getLineNumber() + ":" + line);// 获取行号
+			}
+			System.out.println(lnr.getLineNumber());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void myLineNumberReaderTest() {
+		try (MyLineNumberReader myLnr = new MyLineNumberReader(
+				new FileReader("F:\\study\\9.JAVA_BASE\\笔记\\test.txt"))) {
+			myLnr.setLineNumber(1000);
+			String s;
+			while ((s = myLnr.readLine()) != null) {
+				System.out.println(myLnr.getLineNumber() + ":" + s);
+			}
+			System.out.println(myLnr.getLineNumber());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class MyLineNumberReader extends BufferedReader {
+	private int lineNumber;
+
+	public MyLineNumberReader(Reader in) {
+		super(in);
+		lineNumber = 0;
+	}
+
+	public int getLineNumber() {
+		return lineNumber;
+	}
+
+	public void setLineNumber(int lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+
+	@Override
+	public String readLine() throws IOException {
+		String s;
+		if ((s = super.readLine()) != null) {
+			lineNumber++;
+		}
+		return s;
+	}
+}
