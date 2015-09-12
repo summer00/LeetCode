b209f6cfef287247170f6d1057c8ef2865fb3529
xia <xiaright00@sina.com>
2015-09-12 17:30:46 +0800

    v1.0 add:文件行反向复制

--- base/ioTest/BufferedInputStreamTest.java
+++ base/ioTest/BufferedInputStreamTest.java
@@ -2,10 +2,16 @@ package ioTest;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -60,6 +66,26 @@ public class BufferedInputStreamTest {
 			int b;
 			while ((b = bis.read()) != -1) {
 				bos.write(b);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void exchangeFileLine() {
+		try (BufferedReader br = new BufferedReader(new FileReader("F:\\study\\9.JAVA_BASE\\±Ê¼Ç\\test.txt"));
+				BufferedWriter bw = new BufferedWriter(new FileWriter("F:\\study\\9.JAVA_BASE\\±Ê¼Ç\\test1.txt"));) {
+			String s;
+			List<String> list = new ArrayList<>();
+			while ((s = br.readLine()) != null) {
+				list.add(s);
+			}
+			for (int i = list.size() - 1; i >= 0; i--) {
+				bw.write(list.get(i));
+				bw.newLine();
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
